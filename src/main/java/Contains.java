93847a816b4c21264bc816148e149a4f259aa2a7
eucalvo <eliamcalvo24@gmail.com>
2023-02-20 07:05:09 +0000

    Completed Lab Contains

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,13 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
-        return false;
+        boolean bool = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (target == arr[i]) {
+                bool = true;
+                break;
+            }
+        }
+        return bool;
     }
 }
